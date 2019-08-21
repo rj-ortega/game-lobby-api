23abf1b27887805cc4294f9f70798bbd4addfff2
Rj <rikrdo.ortega@gmail.com>
2019-08-20 22:01:39 -0600

    api structured

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,17 +5,10 @@ class Game < ApplicationRecord
     @games = RestClient::Request.new({
       method: :post,
       url: "https://api-v3.igdb.com/games/",
-      body: "search “zelda”; where rating >= 80;",
+      payload: 'fields id,name,cover.url,platforms.name,screenshots.url,videos.video_id,rating,rating_count,summary,url; where platforms = [48] & rating >= 80 & rating_count >=5 ; sort popularity desc; limit 50;',
       headers: { "user-key": "#{Rails.application.credentials.dig(:games_api_key)}", :accept => :json, content_type: :json },
     }).execute do |response, request, result|
-      case response.code
-      when 400
-        [:error, JSON.parse(response.to_str)]
-      when 200
-        [:success, JSON.parse(response.to_str)]
-      else
-        fail "Invalid response #{response.to_str} received."
-      end
+        JSON.parse(response.to_str)
     end
   end
 end
